44ea96465c7b450c0a3b9764f140fe87e7542790
HelloJacobW <jawoa4d@student.oaisd.org>
2024-10-15 17:49:12 +0000

    Done

--- Program.cs
+++ Program.cs
@@ -3,41 +3,109 @@
 
 public class Program
 {
+    static string name;
+    static string age;
+    static string eyeColor;
+    static string hairColor;
+    static string shoeSize;
+    static string favoriteColor;
+    static string favoriteTVorMovie;
+    static string favoriteTeacher;
+    static string favoriteClass;
+    static string favoriteHoliday;
+    static string favoriteSeason;
+    static string dreamJob;
+    static string numberOfSiblings;
+    static int ageIn5Years;
+    static string x;
+
     public static void Main(string[] args)
     {
-       Console.WriteLine("What is ur name?");
-       String n = Console.ReadLine();
-       Console.WriteLine("What is ur age?");
-       int a = int.Parse(Console.ReadLine());
+        AskName();
+        AskAge();
+        AskEyeColor();
+        AskHairColor();
+        AskShoeSize();
+        AskFavoriteColor();
+        AskFavoriteTvShow();
+        AskFavoriteTeacher();
+        AskFavoriteClass();
+        AskFavoriteHoliday();
+        AskFavoriteSeason();
+        AskDreamJob();
+        CalculateAgeIn5Years(age);
+        AskNumberOfSiblings();
+        DisplaySummaryMessage();
+    }
+    public static void AskName(){
+        Console.WriteLine("What is ur name?");
+        name = Console.ReadLine();
+    }
+    public static void AskAge(){
+        Console.WriteLine("What is ur age?");
+        age = Console.ReadLine();
+    }
+    public static void AskEyeColor(){
        Console.WriteLine("What are ur Eyes colored around da pupils?");
-       String i = Console.ReadLine();
+       eyeColor = Console.ReadLine();
+    }
+    public static void AskHairColor(){
+        Console.WriteLine("What are your hairs colored? because I cannot see you now.");
+        hairColor = Console.ReadLine();
+        if(hairColor.ToLower().Equals("orange"))
+        hairColor = "";
+        else
+        hairColor = "not ";
+    }
+    public static void AskShoeSize(){
        Console.WriteLine("Feet Glove Size?");
-       String s = Console.ReadLine();
+       shoeSize = Console.ReadLine();
+    }
+    public static void AskFavoriteColor(){
        Console.WriteLine("Favorite Color Marble?");
-       String c = Console.ReadLine();
-       String x = "something I could doooooooooooooooo";
-       if(i.Equals(c))
+       favoriteColor = Console.ReadLine();
+       x = "something I could doooooooooooooooo";
+       if(eyeColor.Equals(favoriteColor))
        x = "and";
        else
        x = "but";
+    }
+    public static void AskFavoriteTvShow(){
        Console.WriteLine("Favorite thing to watch on magic box?");
-       String w = Console.ReadLine();
+       favoriteTVorMovie = Console.ReadLine();
+    }
+    public static void AskFavoriteTeacher(){
        Console.WriteLine("Favorite mentor?");
-       String t = Console.ReadLine();
+       favoriteTeacher = Console.ReadLine();
+    }
+    public static void AskFavoriteClass(){
        Console.WriteLine("Best Subject? U do in school.");
-       String ss = Console.ReadLine();
+       favoriteClass = Console.ReadLine();
+    }
+    public static void AskFavoriteHoliday(){
        Console.WriteLine("Funnest Free day? Like Chrismas.");
-       String h = Console.ReadLine();
-       Console.WriteLine("Favorite Season?");
-       String f = Console.ReadLine();
-       if (f.ToLower().Equals("summer"))
-       f = "the worst season";
+       favoriteHoliday = Console.ReadLine();
+    }
+    public static void AskFavoriteSeason(){
+       Console.WriteLine("What your favorite season be?");
+       favoriteSeason = Console.ReadLine();
+       if (favoriteSeason.ToLower().Equals("summer"))
+       favoriteSeason = "the worst season";
+    }
+    public static void AskDreamJob(){
        Console.WriteLine("What are you in future... da thing you do fo money");
-       String j = Console.ReadLine();
+       dreamJob = Console.ReadLine();
+    }
+    public static void CalculateAgeIn5Years(string age){
+        ageIn5Years = (int.Parse(age) + 5);
+    }
+    public static void AskNumberOfSiblings(){
        Console.WriteLine("How many other u's are there. Things u live with. Ssiblings?");
-       String b = Console.ReadLine();
-       Console.WriteLine("\n\n\n Awite Let me Get dis Straight. U are " + n + " whos " + (a + 5) + " years old in 5 yers, but " + a + " years now. You gots " + i + " eyes, " + x + " ur favorite color is " + c + ". Ur over-socks are size " + s + ". You like teacher " + t + ". And to do in school best is " + ss + ". Ur magic box is generally set to " + w + " and you want to be " + j + " when grow up? Im confused? U only has " + b + " Siblings. You like " + h + " and " + f + ". Am I rite?");
-       String yn = Console.ReadLine();
+       numberOfSiblings = Console.ReadLine();
+    }
+    public static void DisplaySummaryMessage(){
+       Console.WriteLine("\n\n\n Awite Let me Get dis Straight. U are " + name + " whos " + ageIn5Years + " years old in 5 yers, but " + age + " years now. You gots " + eyeColor + " eyes, " + x + " ur favorite color is " + favoriteColor + ". Ur over-socks are size " + shoeSize + " and you are " + hairColor + "orange haired. You like teacher " + favoriteTeacher + ". And to do in school best is " + favoriteClass + ". Ur magic box is generally set to " + favoriteTVorMovie + " and you want to be " + dreamJob + " when grow up? Im confused? U only has " + numberOfSiblings + " Siblings. You like " + favoriteHoliday + " and " + favoriteSeason + ". Am I rite?");
+       string yn = Console.ReadLine();
        if(yn.ToLower().Equals("yes"))
        Console.WriteLine("YAY YAY YAY BOSS WILL LIKE DIS!");
        else if(yn.ToLower().Equals("no"))
@@ -45,6 +113,7 @@ public class Program
        else
        Console.WriteLine("I don't get what u say I quit");
     }
+
 }
 /*Best response:
  Awite Let me Get dis Straight. U are John doe whos 84794 years old in 5 yers, but 84789 years now. You gots eleventeen eyes, but ur favorite color is cheese.
